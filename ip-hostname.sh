f03f86ee8bf5cb85190a6ba38a0d4dfeaf4b3471
Suresh <suresh.avvaru123@gmail.com>
2019-09-24 11:54:58 +0530

    updated

--- ip-hostname.sh
+++ ip-hostname.sh
@@ -6,8 +6,8 @@ PVHNAME=$(curl -sL http://169.254.169.254/latest/meta-data/hostname/)
 PBHNAME=$(curl -sL http://169.254.169.254/latest/meta-data/public-hostname/)
 sleep 5s   #"This command is will sleep the script for 5 seconds after that it will show the output"
 
-echo "***My server publicname is $PBHNAME***"
-echo "***privatename is $PVHNAME***"
+echo "***My server publicname is ${PBHNAME}***"
+echo "***privatename is $PVHNAME***"  #we can use variable define with ${} or $ but stick with one style
 echo "***Server publicip is $PVIP***"
 echo "***server privateip is $PIP***"
 

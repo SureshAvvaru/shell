56623d7771f31450191fb698c87ab2c59b2006f3
sureshavvaru <suresh.avvaru123@gmail.com>
2019-09-30 14:33:30 +0000

    added new file

---
+++ delete.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Deleting all files with .sh 
+
+rm -f ../opt*.sh
+
+
+echo "**********************************"
+echo "*Deleted files with .sh extension*"
+echo "**********************************"

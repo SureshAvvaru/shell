295502e22f40e110eb7e9d563ec80f363b2e0ba8
sureshavvaru <suresh.avvaru123@gmail.com>
2019-10-02 12:05:30 +0000

    deleted extra directories

--- backupfiles/backup_user.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Taking backup of user suresh
-USER_SURESH=$(tar -cvf suresh-backup.1.10.2019.tar.gz /home/suresh/)
-DEST_PATH=$(mv suresh-backup.1.10.2019.tar.gz /root)
-
-echo "$USER_SURESH Backup Completed"
-echo "Moved BackupDirectory to Root home directory"
--- s3_backup/s3_backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Sending backup file to s3 bucket
-
-s3_bucket=$(aws s3 cp /root/shell-scripting/suresh.backup-1-10-2019.tar.gz s3://b1-suresh)
-echo "$s3_bucket"
-
-echo "*******************************************"
-echo "*Backup file successfull sent to s3 bucket*"
-echo "*******************************************"
